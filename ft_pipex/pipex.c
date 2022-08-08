aa936a1cea4a6383540d52b27e2b1507d6534fce
RolandVrignon <roland.vrignon@gmail.com>
2022-08-08 14:13:28 +0200

    minor changes

--- ft_pipex/pipex.c
+++ ft_pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 19:25:10 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/08/08 13:19:22 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/08/08 14:12:35 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@ void	close_pipes(int *fd)
 {
 	close(fd[0]);
 	close(fd[1]);
-	close(0);
-	close(1);
-	close(2);
+
 }
 
 void	child_process(char **av, char **envp, int *fd)
 {
 	int	filein;
 
-	filein = open(av[1], O_RDONLY, 0777);
+	filein = open(av[1], O_RDONLY);
 	if (filein == -1)
 	{
 		perror(av[1]);
@@ -42,7 +40,7 @@ void	parent_process(char **av, char **envp, int *fd)
 {
 	int	fileout;
 
-	fileout = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	fileout = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fileout == -1)
 	{
 		perror(av[4]);
@@ -77,8 +75,7 @@ void	process(char **av, char **envp, int fd[2])
 		}
 		if (pid == 0)
 			parent_process(av, envp, fd);
-		if (pid > 0)
-			wait(0);
+		wait(0);
 	}
 }
 
@@ -104,6 +101,7 @@ int	main(int ac, char **av, char **envp)
 			exit(EXIT_FAILURE);
 		}
 		process(av, envp, fd);
+		close_pipes(fd);
 	}
 	else
 		ft_putstr_fd("Too few or too many arguments\n", 2);
